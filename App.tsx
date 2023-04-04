0d146ca65ebe4e1c7649bd664b4e1c4c0e09f654
Ilya Kotsur <99085805+Ilyxxxaa@users.noreply.github.com>
2023-04-04 16:42:53 +0300

    fix: change animation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
 import {AnimatedBall} from './components';
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <AnimatedBall />
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- components/AnimatedBall.tsx
+++ components/AnimatedBall.tsx
@@ -1,27 +1,34 @@
-import {StyleSheet, View, Animated, Pressable} from 'react-native';
+import {StyleSheet, View, Animated, Pressable, Text} from 'react-native';
 import React, {useRef} from 'react';
 
 export const AnimatedBall = () => {
-  const value = useRef(new Animated.ValueXY({x: 0, y: 0})).current;
+  const value = useRef(new Animated.Value(0)).current;
   const moveBall = () => {
     Animated.timing(value, {
-      toValue: {x: 100, y: 100},
-      duration: 1000,
-      useNativeDriver: false,
+      toValue: 1,
+      duration: 5000,
+      useNativeDriver: true,
     }).start();
   };
 
   return (
-    <View>
-      <Animated.View style={value.getLayout()}>
+    <View style={styles.container}>
+      <Animated.View style={{opacity: value}}>
         <View style={styles.redCircle} />
       </Animated.View>
-      <Pressable style={styles.btn} onPress={moveBall} />
+      <Pressable style={styles.btn} onPress={moveBall}>
+        <Text>Click me!</Text>
+      </Pressable>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   redCircle: {
     backgroundColor: 'red',
     width: 100,
